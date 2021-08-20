f68106895e5df2b6c618056e38470b4b6bc24598
Brian <briancbarrow@gmail.com>
2021-08-20 11:47:37 -0600

    fixed how i get author xml feed

--- pages/blog/authors/_author.vue
+++ pages/blog/authors/_author.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+  <div class="px-4 pt-16 pb-20 bg-white sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
     <div
       class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl"
     >
       <div>
         <h2
-          class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"
+          class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl"
         >
           {{ author }}
         </h2>
@@ -13,7 +13,7 @@
           class="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center"
         ></div>
       </div>
-      <div class="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
+      <div class="grid gap-16 pt-10 mt-6 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
         <div v-for="post in posts" :key="post.title">
           <p class="text-sm text-gray-500">
             <time :datetime="post.datetime">{{ post.date | date }}</time>
